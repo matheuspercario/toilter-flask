07fd5e28d57d544dd1cc70b877cff6458a70af31
Matheus Bruder <matheuspercario@gmail.com>
2020-11-11 12:25:31 -0300

    Criando rotas (URLs)

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,26 @@
 from app import app
 
+# Teste hello world
+@app.route("/hello/")
+def hello():
+    return "Hello World!"
 
+# Teste Index
+@app.route("/index/")
 @app.route("/")
 def index():
-    return "Hello world!"
+    return "Index Page"
+
+# Teste parâmetro
+@app.route("/test/", defaults={'name': None})
+@app.route("/test/<name>/")
+def test(name):
+    if name:
+        return f"Olá, {name}!"
+    else:
+        return f"Olá, usuário!"
+
+# Teste parâmetro numérico
+@app.route("/test2/<int:id>/")
+def test2(id):
+    return f"O ID é {id}"
