3a3094d9e65048297c7540b8146c54aa818a3b57
Matheus Bruder <matheuspercario@gmail.com>
2020-11-11 16:22:45 -0300

    Views: arquivos estaticos e templates

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,15 +1,36 @@
+from flask import render_template
 from app import app
+
+
+# Página Index
+@app.route('/index/')
+@app.route('/',)
+def index():
+    return render_template('index.html',)
+
+
+@app.route('/login/')
+def login():
+    return render_template('base.html')
+
+
+# ------------------------------------------------------------
+# TESTES -----------------------------------------------------
 
 # Teste hello world
 @app.route("/hello/")
 def hello():
-    return "Hello World!"
+    return """
+    <html>
+    <head>
+        <title>Hello world</title>
+    </head>
+    <body>
+        <h1>Hello world!</h1>
+    </body>
+    </html>
+    """
 
-# Teste Index
-@app.route("/index/")
-@app.route("/")
-def index():
-    return "Index Page"
 
 # Teste parâmetro
 @app.route("/test/", defaults={'name': None})
@@ -21,6 +42,8 @@ def test(name):
         return f"Olá, usuário!"
 
 # Teste parâmetro numérico
+
+
 @app.route("/test2/<int:id>/")
 def test2(id):
     return f"O ID é {id}"
